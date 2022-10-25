7f1b38758fb872caab96530c804a172a16735bf1
Sebafleaca <sebastian.fleaca@hotmail.com>
2022-10-25 22:12:40 +0200

    Create database for users

--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+
+    user_name = models.CharField(max_length=200)
+    user_password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return  self.user_name
