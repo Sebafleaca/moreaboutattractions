ff79dafd3b65975f1c55982d471b95961b4bc341
Sebafleaca <sebastian.fleaca@hotmail.com>
2022-10-25 02:18:17 +0200

    Create routes

--- attractions/urls.py
+++ attractions/urls.py
@@ -7,4 +7,7 @@ app_name = 'attractions'
 
 urlpatterns = [
     path('', views.home, name='attractions'),
+    path('nearby', views.nearby, name='nearby'),
+    path('found', views.found, name='found'),
+    path('favorites', views.favorites, name='favorites'),
 ]
--- attractions/views.py
+++ attractions/views.py
@@ -8,3 +8,29 @@ def home(request):
         'variable': variable
     }
     return render(request, 'attractions/home.html', context)
+
+def nearby(request):
+    x = 1
+    y = 2
+    posi = [x,y]
+
+    context = {
+        'posi': posi
+    }
+    return render(request, 'attractions/nearby.html', context)
+
+def found(request):
+    fav_attractions = ['a','b','c']
+
+    context = {
+        'fav_attractions': fav_attractions
+    }
+    return render(request, 'attractions/found.html', context)
+
+def favorites(request):
+    fav_attractions = ['a','b','c']
+
+    context = {
+        'fav_attractions': fav_attractions
+    }
+    return render(request, 'attractions/favorites.html', context)
