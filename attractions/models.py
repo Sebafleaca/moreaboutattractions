e9f5ad7f99d2cdc2ab36af8aaff39853a047df4a
Sebafleaca <sebastian.fleaca@hotmail.com>
2022-10-25 08:08:39 +0200

    Create database

--- attractions/models.py
+++ attractions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Attraction(models.Model):
+    name = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    distance = models.FloatField(default=0.0)
+    saved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+        
